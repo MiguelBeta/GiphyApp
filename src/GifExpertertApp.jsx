f4c3fd027003d1d101a12907a83d9b75cb6f134d
Miguel <workingbeta01@gmail.com>
2024-01-12 16:28:31 -0500

    First Commit

---
+++ src/GifExpertertApp.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export const GifExpertApp = () => {
+
+
+    const [ categories, setCategories ] = useState([ 'One Punch' ]);
+    console.log(categories);
+
+    return (
+        
+        <>
+            {/* Titulo */}
+            <h1>GifExpertApp</h1>
+
+            {/* Input */}
+
+            {/* Listado de Gif */}
+                {/* Gif Item */}
+        </>
+        
+        );
+}
+
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+import { GifExpertApp } from './GifExpertertApp.jsx'
+import './style.css';
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GifExpertApp />
+  </React.StrictMode>,
+); 
